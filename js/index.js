5d441494d26f841ce5e91b48ab331adec4d3a79b
Antony Chavez <ivanchr2199@outlook.com>
2021-08-03 10:52:51 -0500

    implementation of the main game logic

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
+import drawPokecards from "./drawPokecards.js";
+import generatePokemons from "./generatePokemons.js";
+import gameCompleted from "./gameCompleted.js";
+import fireworks from "./fireworks.js";
+import checkMatch from "./checkMatch.js"
+const pokemons = generatePokemons(6);
+drawPokecards(pokemons);
+
+const $allCards = document.getElementsByClassName("card"),
+var $cardSelectedA = undefined,
+    $cardSelectedB = undefined;
 document.addEventListener("click", e =>{
-    const $card = document.querySelector(".card")
-    const $front = document.querySelector(".card_front")
-    const $back = document.querySelector(".card_back")
-    if (e.target.parentElement == $card){
-        $card.classList.toggle("card--show");
-        console.log($card)
+    if(
+        e.target.parentElement.classList.contains("card") && 
+        e.target.parentElement.classList.contains("resolved") == false
+    ){
+        let $clickedCard = e.target.parentElement;
+        $clickedCard.classList.add("card--show");
+        if($cardSelectedA == undefined){
+            $cardSelectedA = $clickedCard;
+        }else{
+            if($cardSelectedA !== $clickedCard){
+                $cardSelectedB = $clickedCard;
+                [$cardSelectedA, $cardSelectedB] = checkMatch($cardSelectedA, $cardSelectedB);
+            }
+        }
     }
-})
+    gameCompleted($allCards)? fireworks(): null;
+});
+
+
+
+
