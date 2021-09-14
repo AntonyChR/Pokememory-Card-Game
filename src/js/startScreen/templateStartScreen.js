9fea0eeaf8eeb6fb414f83470ee092d196fe40c7
Antony Chavez <ivanchr2199@outlook.com>
2021-09-14 15:26:46 -0500

    html template - starting page

---
+++ src/js/startScreen/templateStartScreen.js
@@ -0,0 +1,10 @@
+
+export const templateStartScreen = `
+        <div class="input-name_container">
+            <form class="nick-name_form">
+                <label class="input-name_label" for="input-name">¿Cuál es tu Nombre?</label>
+                <input required autocomplete="off" id="input-name" type="text"></input>
+                <button class="button-name">A jugar</button>
+            </form>
+        </div>
+`
