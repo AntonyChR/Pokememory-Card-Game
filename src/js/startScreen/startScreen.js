2a80516f5f23dded4d31c04eedbd449bd89aff01
Antony Chavez <ivanchr2199@outlook.com>
2021-09-14 15:20:47 -0500

    starting screen

---
+++ src/js/startScreen/startScreen.js
@@ -0,0 +1,17 @@
+import { templateStartScreen } from "./templateStartScreen.js";
+
+export const startScreen = new Promise((resolve, reject) => {
+    let $fragment = document.createElement("div");
+    $fragment.classList.add("start-screen");
+    $fragment.innerHTML = templateStartScreen;
+    document.querySelector(".body").prepend($fragment);
+    const getName = (e) => {
+        e.preventDefault();
+        let nickName = document.querySelector("#input-name").value;
+        if (nickName) resolve(nickName);
+        else reject("xd");
+        document.removeEventListener("submit", getName);
+    };
+
+    document.addEventListener("submit", getName);
+});
