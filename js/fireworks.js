4e37912c05518968bf07fda99416b0e210bc0fa6
Antony Chavez <ivanchr2199@outlook.com>
2021-09-14 15:03:41 -0500

    moved

--- js/fireworks.js
+++
@@ -1,3 +0,0 @@
-export default function fireworks(){
-    console.log("Congratulations")
-}
--- js/hideCards.js
+++
@@ -1,5 +0,0 @@
-export default function hideCard($cardA, $cardB){
-    document.querySelector(".error__sound").play()
-    $cardB.classList.remove("card--show");
-    $cardA.classList.remove("card--show");
-}
