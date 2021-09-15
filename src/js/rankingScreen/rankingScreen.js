1ac5d2ec3150522ee5db598760b566fb79ffd534
Antony Chavez <ivanchr2199@outlook.com>
2021-09-15 12:51:09 -0500

    created functinos that show rankig

---
+++ src/js/rankingScreen/rankingScreen.js
@@ -0,0 +1,22 @@
+import { getScores } from "../firebase/getScores";
+import templateRankingtable from "./templateRankingTable";
+
+export const rankingScreen =async() =>{
+    const $rankingContainer = document.createElement('div');
+    $rankingContainer.classList.add("end-screen");
+    $rankingContainer.classList.add("hidden");
+
+    $rankingContainer.innerHTML = templateRankingtable;
+    document.querySelector('.body').prepend($rankingContainer);
+    let tableElements = '';
+    getScores().then(players =>{
+        players.forEach(player => {
+            tableElements+=`<tr><th>${player.Player}</th> <th>${player.score}</th></tr>`
+        })
+        document.querySelector('.body-table').innerHTML = tableElements; 
+        $rankingContainer.classList.toggle('hidden');
+    })
+
+    
+
+}
