01cf94dee47688e50718cc878ece6c8de36b2425
Antony Chavez <ivanchr2199@outlook.com>
2021-09-15 12:50:24 -0500

    create html template for ranking

---
+++ src/js/rankingScreen/templateRankingTable.js
@@ -0,0 +1,16 @@
+const templateRankingtable = `
+        <div class="scores-container ">
+            <table class="scores_table">
+                <thead class="header-table">
+                    <tr>
+                        <th>Player</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                    <tbody class="body-table">
+                    </tbody>
+            </table>
+        </div>
+`;
+
+export default templateRankingtable;
