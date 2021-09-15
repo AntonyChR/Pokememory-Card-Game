24c1ceb41fc8fd8b2bdb96e050233784f8b78033
Antony Chavez <ivanchr2199@outlook.com>
2021-09-15 12:51:45 -0500

    call rankingScreen function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import { startScreen } from "./js/startScreen/startScreen.js";
 import { mainLogic } from "./js/cardsScreen/mainLogic.js";
 import { registerScore } from "./js/firebase/registerScore.js";
+import { rankingScreen } from "./js/rankingScreen/rankingScreen.js";
 
 startScreen.then((playerName)=>{
     document.querySelector('.start-screen').remove();
     document.querySelector('.cards-container').classList.toggle('hidden');
     mainLogic.then((score)=> {
         registerScore(playerName,score);
-        //selebration song
-        //show all scores -> with retart buttom
-
+        rankingScreen();
     });
 });
 
