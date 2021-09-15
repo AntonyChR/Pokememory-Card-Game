3bc0309cfe918e7903d2df2611d65eb01efcfd2b
Antony Chavez <ivanchr2199@outlook.com>
2021-09-15 13:03:53 -0500

    directory restructuring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { startScreen } from "./js/startScreen/startScreen.js";
-import { mainLogic } from "./js/cardsScreen/mainLogic.js";
-import { registerScore } from "./js/firebase/registerScore.js";
-import { rankingScreen } from "./js/rankingScreen/rankingScreen.js";
+import { startScreen } from "./startScreen/startScreen.js";
+import { mainLogic } from "./cardsScreen/mainLogic.js";
+import { registerScore } from "./firebase/registerScore.js";
+import { rankingScreen } from "./rankingScreen/rankingScreen.js";
 
 startScreen.then((playerName)=>{
     document.querySelector('.start-screen').remove();
