c9b2171575931ae21ba04280add163fb56813e2a
Antony Chavez <ivanchr2199@outlook.com>
2021-09-15 12:49:56 -0500

    Change button text

--- src/js/startScreen/templateStartScreen.js
+++ src/js/startScreen/templateStartScreen.js
@@ -4,7 +4,7 @@ export const templateStartScreen = `
             <form class="nick-name_form">
                 <label class="input-name_label" for="input-name">¿Cuál es tu Nombre?</label>
                 <input required autocomplete="off" id="input-name" type="text"></input>
-                <button class="button-name">A jugar</button>
+                <button class="button-name">Go!!</button>
             </form>
         </div>
 `
