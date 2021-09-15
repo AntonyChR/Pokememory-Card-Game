7ebdfaa585ff3897957c1e7a2e92816109a1873a
Antony Chavez <ivanchr2199@outlook.com>
2021-09-15 12:40:38 -0500

    Babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
+    devtool: 'source-map',
+    mode: 'development',
+    entry: ["@babel/polyfill",'./src/index.js'],
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'Public')
+    },
+    devServer: {
+        port: 3000,
+        static: path.join(__dirname, 'Public'),
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            }
+        ]
     }
+
 }
