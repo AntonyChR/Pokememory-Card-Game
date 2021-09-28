f0b17110f2fc9596d96d9761ac96ef6da3f194b3
Antony Chavez <ivanchr2199@outlook.com>
2021-09-27 20:40:43 -0500

    return 10 elements from history score

--- src/firebase/getScores.js
+++ src/firebase/getScores.js
@@ -1,12 +1,12 @@
 import db from "./firebaseConfig";
 import { getDocs, collection } from "@firebase/firestore";
 
-export const getScores = async () => {
+export const getScores = async (numberElements=10) => {
     const scoresSnap = await getDocs(collection(db, "scores"));
     let scoreHistory = [];
     scoresSnap.forEach((player) => {
         scoreHistory.push(player.data());
     });
     scoreHistory.sort((a, b) => a.score - b.score);
-    return scoreHistory;
+    return scoreHistory.slice(0,numberElements);
 };
