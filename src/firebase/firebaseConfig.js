49bc08e1d8de559c0d2462ebd8edfab129636794
Antony Chavez <ivanchr2199@outlook.com>
2021-09-27 20:44:35 -0500

    firebase config

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,15 +2,12 @@ import { initializeApp } from "@firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-/*  
-    ------------> firebase configuration <----------------  
-    apiKey
-    authDomain
-    projectId
-    storageBucket
-    messagingSenderId
-    appId
-*/
+    apiKey: "AIzaSyDWAihX0fGNEw0yzPdpjXPmVK4jOplrXgg",
+    authDomain: "pokememory-75b6d.firebaseapp.com",
+    projectId: "pokememory-75b6d",
+    storageBucket: "pokememory-75b6d.appspot.com",
+    messagingSenderId: "253441683005",
+    appId: "1:253441683005:web:f90d76d5b62ae9c6dd0454",
 };
 
 // Initialize Firebase
